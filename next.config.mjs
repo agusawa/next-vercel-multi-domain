be580a608b822d31c760d4e59f8fd2e31757a4f6
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-08-12 14:34:12 +0700

    Refactor middleware to handle career.example.agus.stiawan.site redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/career/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'career.example.agus.stiawan.site',
-          },
-        ],
-      },
-      {
-        source: '/',
-        destination: '/career',
-        has: [
-          {
-            type: 'host',
-            value: 'career.example.agus.stiawan.site',
-          },
-        ],
-      },
-      {
-        source: '/:path*',
-        destination: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'example.agus.stiawan.site',
-          },
-        ],
-      },
-    ]
-  }
-};
-
-export default nextConfig;
+    async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: '/career',
+          has: [
+            {
+              type: 'host',
+              value: 'career.example.agus.stiawan.site',
+            },
+          ],
+        },
+        {
+          source: '/:path*',
+          destination: '/career/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'career.example.agus.stiawan.site',
+            },
+          ],
+        },
+        {
+          source: '/:path*',
+          destination: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'example.agus.stiawan.site',
+            },
+          ],
+        },
+      ]
+    }
+  };
+  
+  export default nextConfig;
+  
--- src/middleware.ts
+++ src/middleware.ts
@@ -11,10 +11,8 @@ export function middleware(request: NextRequest) {
       return NextResponse.rewrite(url);
     }
 
-    if (!url.pathname.startsWith("/career")) {
-      url.pathname = `/career${url.pathname}`;
-      return NextResponse.rewrite(url);
-    }
+    url.pathname = `/career${url.pathname}`;
+    return NextResponse.rewrite(url);
   }
 
   // Handle requests to example.agus.stiawan.site
