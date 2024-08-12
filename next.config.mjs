ecf24879f001f9606609b294a9722f0d5eff3f5d
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-08-12 15:00:50 +0700

    Refactor middleware to handle career.example.agus.stiawan.site redirects

--- next.config.mjs
+++ next.config.mjs
@@ -14,36 +14,6 @@ const nextConfig = {
             ],
           },
           {
-            source: '/contact-us',
-            destination: '/career/contact-us',
-            has: [
-              {
-                type: 'host',
-                value: 'career.example.agus.stiawan.site',
-              },
-            ],
-          },
-          {
-            source: '/about-us',
-            destination: '/career/about-us',
-            has: [
-              {
-                type: 'host',
-                value: 'career.example.agus.stiawan.site',
-              },
-            ],
-          },
-          {
-            source: '/:slug',
-            destination: '/career/:slug',
-            has: [
-              {
-                type: 'host',
-                value: 'career.example.agus.stiawan.site',
-              },
-            ],
-          },
-          {
             source: '/:path*',
             destination: '/career/:path*',
             has: [
