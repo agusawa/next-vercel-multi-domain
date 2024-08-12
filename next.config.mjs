379f9350919e7e5e6d5dd2b17296cd63f5902ab1
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-08-12 13:51:14 +0700

    Update rewrite rules for career pages and homepage

--- next.config.mjs
+++ next.config.mjs
@@ -3,26 +3,16 @@ const nextConfig = {
     async rewrites() {
       return [
         {
-          source: '/career/:path*',
-          destination: '/career/:path*',
-          basePath: false,
-          has: [
-            {
-              type: 'host',
-              value: 'career.example.agus.stiawan.site',
-            },
-          ],
+          source: '/career/:slug',
+          destination: 'https://career.example.agus.stiawan.site/:slug',
+        },
+        {
+          source: '/career',
+          destination: 'https://career.example.agus.stiawan.site',
         },
         {
           source: '/:path*',
-          destination: '/:path*',
-          basePath: false,
-          has: [
-            {
-              type: 'host',
-              value: 'example.agus.stiawan.site',
-            },
-          ],
+          destination: 'https://example.agus.stiawan.site/:path*',
         },
       ]
     }
