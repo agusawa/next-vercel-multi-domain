640be44883314950272a00aa632ebd3be53aef5c
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-08-12 14:55:31 +0700

    Refactor middleware to handle career.example.agus.stiawan.site redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,68 +1,70 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
-      return [
-        {
-          source: '/',
-          destination: '/career',
-          has: [
-            {
-              type: 'host',
-              value: 'career.example.agus.stiawan.site',
-            },
-          ],
-        },
-        {
-          source: '/contact-us',
-          destination: '/career/contact-us',
-          has: [
-            {
-              type: 'host',
-              value: 'career.example.agus.stiawan.site',
-            },
-          ],
-        },
-        {
-          source: '/about-us',
-          destination: '/career/about-us',
-          has: [
-            {
-              type: 'host',
-              value: 'career.example.agus.stiawan.site',
-            },
-          ],
-        },
-        {
-          source: '/:slug',
-          destination: '/career/:slug',
-          has: [
-            {
-              type: 'host',
-              value: 'career.example.agus.stiawan.site',
-            },
-          ],
-        },
-        {
-          source: '/:path*',
-          destination: '/career/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'career.example.agus.stiawan.site',
-            },
-          ],
-        },
-        {
-          source: '/:path*',
-          destination: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'example.agus.stiawan.site',
-            },
-          ],
-        },
-      ]
+      return {
+        beforeFiles: [
+          {
+            source: '/',
+            destination: '/career',
+            has: [
+              {
+                type: 'host',
+                value: 'career.example.agus.stiawan.site',
+              },
+            ],
+          },
+          {
+            source: '/contact-us',
+            destination: '/career/contact-us',
+            has: [
+              {
+                type: 'host',
+                value: 'career.example.agus.stiawan.site',
+              },
+            ],
+          },
+          {
+            source: '/about-us',
+            destination: '/career/about-us',
+            has: [
+              {
+                type: 'host',
+                value: 'career.example.agus.stiawan.site',
+              },
+            ],
+          },
+          {
+            source: '/:slug',
+            destination: '/career/:slug',
+            has: [
+              {
+                type: 'host',
+                value: 'career.example.agus.stiawan.site',
+              },
+            ],
+          },
+          {
+            source: '/:path*',
+            destination: '/career/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'career.example.agus.stiawan.site',
+              },
+            ],
+          },
+          {
+            source: '/:path*',
+            destination: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'example.agus.stiawan.site',
+              },
+            ],
+          },
+        ]
+      }
     }
   };
   
